8edab1d6932a3076d53e21acffef5a5fed7ef2a1
liuwf <m17600576201@163.com>
2018-12-11 19:04:27 +0800

    2018/12/11

--- main/common/readconfig.py
+++ main/common/readconfig.py
@@ -4,7 +4,7 @@
 
 import os
 import logging
-from .yaml_read import YamlReader
+from yaml_read import YamlReader
 # 所有相关文件的路径
 # 通过当前文件的绝对路径，其父级目录一定是框架的base目录，然后确定各层的绝对路径。如果你的结构不同，可自行修改。
 # 之前直接拼接的路径，修改了一下，用现在下面这种方法，可以支持linux和windows等不同的平台，也建议大家多用os.path.split()和os.path.join()，不要直接+'\\xxx\\ss'这样
@@ -19,14 +19,15 @@ REPORT_PATH = os.path.join(BASE_PATH, 'report')
 
 
 class Config:
-    def __init__(self, config = CONFIG_FILE):
+    def __init__(self, config = CONFIG_FILE, index=1, element = "home"):
             logging.warning('配置文件地址：',config)
             self.config = YamlReader(config).data
-    def get(self, element, index=0):
+    def get(self, element, index):
         return self.config[index].get(element)
 
 
 
 
 if __name__ == '__main__':
-    pass
+    c = Config()
+    print(c.config)
